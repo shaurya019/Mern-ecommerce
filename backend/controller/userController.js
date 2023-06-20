d39a7a2b7a4c98d547481d42a248eb65c15a517a
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-06-21 03:10:25 +0530

    Cookie and token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,7 @@
 const ErrorHander = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
 const User = require("../models/userModel");
-
+const sendToken = require("../utils/jwtToken")
 
 // Register a User
 
@@ -19,8 +19,35 @@ exports. registerUser = catchAsyncErrors(async (req,res,next) =>{
       });
       const token = user.getJWTToken();
 
-      res.status(200).json({
-        success:true,
-        token, 
-      });
+      sendToken(user, 201, res);
+});
+
+
+
+// Login User
+
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHander("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const token = user.getJWTToken;
+  
+  sendToken(user, 200, res);
 });
--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -65,6 +65,10 @@ userSchema.methods.getJWTToken = function () {
 };
 
 
+// Compare Password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 
 module.exports = mongoose.model("User", userSchema);
