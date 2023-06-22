c4c5bb00ef42a240906856819ee3c9ac0e045635
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-06-22 18:50:03 +0530

    error handle and reset password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -116,7 +116,31 @@ try {
 exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
  
   // creating token hash
- 
+  const resetPasswordToken = crypto
+  .createHash("sha256")
+  .update(req.params.token)
+  .digest("hex");
+
+
+  const user = await user.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt :Date.now() },
+  });
+
+
+  if (!user) {
+    return next(new ErrorHander("Reset Password token is Invalid", 404));
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHander("Password does not password", 400));
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
 
   sendToken(user, 200, res);
 });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -10,6 +10,8 @@ router.route("/login").post(loginUser);
 
 router.route("/password/forgot").post(forgotPassword);
 
+router.route("/password/reset/:token").put(resetPassword);
+
 router.route("/logout").post(logout);
 
 module.exports = router;
