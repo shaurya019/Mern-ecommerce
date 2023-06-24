eeb764039da43a77b01716e4adda0c3d139b0e38
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-06-24 14:30:11 +0530

    Product_Card

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,7 +1,14 @@
 import React, { Fragment, useEffect } from "react";
 import { CgMouse } from "react-icons/all";
-import ProductCard from "./ProductCard.js";
+import Product from "./ProductCard.js";
 import "./Home.css";
+
+const product = {
+    name:"Blue Tshirt",
+    images: [{url:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Ftshirt%2F&psig=AOvVaw00XFn8mQE35_JEkBq_cWQG&ust=1687683303709000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPCO76_E2_8CFQAAAAAdAAAAABAE"}],
+    price:"Rs3000",
+    _id:"singh"
+};
 const Home = () => {
   return (
     <Fragment>
@@ -17,7 +24,15 @@ const Home = () => {
           </div>
           <h2 className="homeHeading">Featured Products</h2>
           <div className="container" id="container">
-            {/* <Product product={product} /> */}
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
           </div>
       </Fragment>
   )
