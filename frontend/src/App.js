e0baebbab525bbdbc46a09c2a50cbe5642cc6aa2
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-06-24 14:12:48 +0530

    Frontend - setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import {Routes, Route} from 'react-router-dom';
+import Header from "./component/Layout/Header/Header.js";
+import Footer from "./component/Layout/Footer/Footer.js";
+import WebFont from "webfontloader";
+import Home from "./component/Home/Home.js"
 import './App.css';
 
 function App() {
+  
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      
-    </div>
+   <>
+    <Header />
+  <Routes>
+  <Route path='/' element={<Home/>} />
+  </Routes>
+  <Footer />
+  </>
   );
 }
 
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,26 @@
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/all";
+import ProductCard from "./ProductCard.js";
+import "./Home.css";
+const Home = () => {
+  return (
+    <Fragment>
+       <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {/* <Product product={product} /> */}
+          </div>
+      </Fragment>
+  )
+}
+
+export default Home
